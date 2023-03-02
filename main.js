94aa738dcc24c208344695d0cb0c1322a670cf8f
nothingwastaken <82754118+nothingwastaken@users.noreply.github.com>
2023-03-02 18:02:08 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
+status = ""
+video = ""
 
+function preload(){
+    video = createCapture(VIDEO);
+    video.size(600,400);
+    video.hide();
+}
 
 function setup(){
-    canvas = createCanvas(600, 400);
+    canvas = createCanvas(550, 400);
     canvas.center();
 }
 
 function draw(){
+    image(video,0,0, 550, 400)
+}
 
+function Start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting objects";
+    x = document.getElementById('input').value;
+    console.log(x);
+}
+
+function modelLoaded(){
+    status = true;
+    console.log("working");
 }
