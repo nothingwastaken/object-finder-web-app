68744d10b5cd42ee6e43da896e23c429a6100653
nothingwastaken <82754118+nothingwastaken@users.noreply.github.com>
2023-03-11 13:38:03 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-status = ""
-video = ""
+var stati = " ";
+video = "";
+objects = [];
 
 function preload(){
     video = createCapture(VIDEO);
@@ -14,6 +15,21 @@ function setup(){
 
 function draw(){
     image(video,0,0, 500, 400)
+    console.log(stati);
+    if (stati != " "){
+        objectDetector.detect(video, gotResult);
+        for (i=0; i < object.length; i++){
+            document.getElementById("status").innerHTML = "Status: object detected.";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected is: " + objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function Start(){
@@ -24,6 +40,14 @@ function Start(){
 }
 
 function modelLoaded(){
-    status = true;
+    stati = true;
+    console.log(stati);
     console.log("working");
+}
+
+function gotResult(error, results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
 }
